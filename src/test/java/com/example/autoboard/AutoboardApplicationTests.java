95cca29f79d607199048e0f665aa17394bb3e00e
Mukhunyeledzi-Muthuphei <Mukhunyeledzi.Muthuphei@bbd.co.za>
2025-03-09 10:31:38 +0200

    Initial commit

---
+++ src/test/java/com/example/autoboard/AutoboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
