c57d635f15fe9cef9e865e1dba2b762ab3b4cc35
Mukhunyeledzi-Muthuphei <Mukhunyeledzi.Muthuphei@bbd.co.za>
2025-03-11 22:16:51 +0200

    api fixed

--- api/src/main/java/com/example/autoboard/AutoboardApplication.java
+++ api/src/main/java/com/example/autoboard/AutoboardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutoboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GoogleAuthCommands.class, args);
+		SpringApplication.run(AutoboardApplication.class, args);
 	}
 
 }
