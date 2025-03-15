6df0984fe1243ef5107f2c7362cfcc313d93817b
rdngobeni <rivo.ngobeni@standardbank.co.za>
2025-03-15 19:23:06 +0200

    allow all traffic from external calls

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,6 +96,7 @@ resource "aws_security_group" "rds_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     security_groups = [aws_security_group.beanstalk_sg.id] #allow traffic from beanstalk.
   }
 
