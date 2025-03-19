de4b9e556304c40c95f345d39a0652eeb629a7ab
rdngobeni <rivo.ngobeni@standardbank.co.za>
2025-03-19 12:35:41 +0200

    Add env variables to workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,14 @@
 variable "db_username" {
   description = "RDS master username"
   type        = string
+  default     = try(env("DB_USERNAME"), "")
 }
 
 variable "db_password" {
   description = "RDS master password"
   type        = string
   sensitive   = true
+  default     = try(env("DB_PASSWORD"), "")
 }
 
 variable "region" {
@@ -18,6 +20,7 @@ variable "region" {
 variable "google_client_secret" {
   description = "Google Client Secret"
   type        = string
-  sensitive = true
+  sensitive   = true
+  default     = try(env("GOOGLE_SECRET"), "")
 }
 
